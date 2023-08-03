d377a0c696c20757441f22ff9328293a826a72a5
archna94 <126779485+archna94@users.noreply.github.com>
2023-08-03 16:48:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,7 @@ provider "aws" {
     region = "us-east-1"
 
 }
-terraform {
-  backend "s3" {
-    bucket = "archu9876"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
